3da947f856611da5deb3a2cd27885787e2e5fc5c
Meryl-Fang <mfang@wellesley.edu>
2024-02-17 19:50:13 -0500

    add load_data, compute_analysis,notify_done

--- Analysis.py
+++ Analysis.py
@@ -2,7 +2,15 @@ from typing import Any, Optional
 import matplotlib.pyplot as plt
 import yaml
 import requests
+import json
+import pandas as pd
+from datetime import datetime
+import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[logging.StreamHandler(), logging.FileHandler('nasa_neows_assignment.log')],
+)
 
 class Analysis():
 
@@ -11,7 +19,7 @@ class Analysis():
 
         # add the analysis config to the list of paths to load
         paths = CONFIG_PATHS + [analysis_config]
-
+        logging.info(f'Loading Configs from and sequentially from {paths}')
         # initialize empty dictionary to hold the configuration
         config = {}
 
@@ -24,15 +32,87 @@ class Analysis():
         self.config = config
 
     def load_data(self) -> None:
-        data = requests.get('/url/to/data').json()
-        self.dataset = data
+        ''' Retrieve data from NASA's Near Earth Object Web Service (NeoWs) API
 
-    def compute_analysis(self) -> Any:
-        return self.dataset.mean() 
+        This function makes an HTTPS request to the NeoWs API and retrieves your selected data. 
+        The data is stored in the Analysis object.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+
+        '''
+        start_date = self.config["start_date"]
+        end_date = self.config["end_date"]
+        
+        try:
+            raw_data = requests.get(f'https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={self.config["API_KEY"]}').json()
+            if raw_data.get("code") == 400:
+                print(raw_data["http_error"],raw_data["error_message"])
+            data = []
+            for (date, value_list) in raw_data["near_earth_objects"].items():
+                data += value_list
+            self.dataset = pd.DataFrame(data)
+            logging.info(f"Data from {start_date} to {end_date} succesfully loaded.")
+        except Exception as e:
+            logging.error(f"Error while loading data: {e}")
+        
+        
+
+    def compute_analysis(self) -> float:
+        '''Analyze previously-loaded data.
+
+        This function runs an analytical measure computing
+        the mean of the absolute_magnitude_h of the asteroids in the loaded dataset
+        and returns the statistics in float.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        analysis_output : float
+
+        '''
+        try:
+            mean = self.dataset["absolute_magnitude_h"].mean()
+            logging.info(f"Mean of absolute_magnitude_h: {mean}")
+            return mean
+        except Exception as e:
+            logging.error(f"Error when computing mean: {e}")
 
     def plot_data(self, save_path: Optional[str] = None) -> plt.Figure:
         pass
 
     def notify_done(self, message: str) -> None:
-        pass
+        ''' Notify the user that analysis is complete.
+
+        Send a notification to the user through the ntfy.sh webpush service.
+
+        Parameters
+        ----------
+        message : str
+        Text of the notification to send
+
+        Returns
+        -------
+        None
+
+        '''
+        topic = 'nasa-neows-assignment'
+        message = message
+
+        # send a message through ntfy.sh
+        requests.post(
+            'https://ntfy.sh/' + topic,
+            data=message.encode('utf-8'),
+        )
     
+nasa_neows = Analysis("configs/job_file.yml")
+nasa_neows.load_data()
+nasa_neows.compute_analysis()
